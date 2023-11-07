e0c1a83a3c01da6d6e8d24febb177c8cd0d46eee
dinh <dinhtq170601@gmail.com>
2023-11-07 18:05:02 +0700

    edit baseUrl

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.API_URL || "https://e-learning-95lab-productionv1.onrender.com/v1/api",
+  baseURL: "https://e-learning-95lab-productionv1.onrender.com/v1/api" || process.env.API_URL,
   headers: {
     "Content-Type": "application/json",
     "x-api-key":
