c2e1636622e1cb2488569f29b456b9767bcd9609
dinh <dinhtq170601@gmail.com>
2023-11-22 19:52:37 +0700

    fix call stack

--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -1,27 +1,27 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { Typography } from "@material-tailwind/react";
-import StatisticsCard from "@/components/Card/statistics-card";
-import {
-  BanknotesIcon,
-  ChartBarIcon,
-  ClockIcon,
-  UserPlusIcon,
-  UsersIcon,
-} from "@heroicons/react/24/solid";
-import statisticsCardsData from "@/data/statistics-cards-data";
-import statisticsChartsData from "@/data/statistics-charts-data";
-import dynamic from "next/dynamic";
+import React from "react";
+// import { Typography } from "@material-tailwind/react";
+// import StatisticsCard from "@/components/Card/statistics-card";
+// import {
+//   BanknotesIcon,
+//   ChartBarIcon,
+//   ClockIcon,
+//   UserPlusIcon,
+//   UsersIcon,
+// } from "@heroicons/react/24/solid";
+// import statisticsCardsData from "@/data/statistics-cards-data";
+// import statisticsChartsData from "@/data/statistics-charts-data";
+// import dynamic from "next/dynamic";
 
-const StatisticsChartDynamic = dynamic(
-  () => import('@/components/Chart/statistics-chart'),
-  { ssr: false }
-)
+// const StatisticsChartDynamic = dynamic(
+//   () => import('@/components/Chart/statistics-chart'),
+//   { ssr: false }
+// )
 
 export default function Dashboard() {
   return (
     <div className="mt-12">
-      <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
+      {/* <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
         {statisticsCardsData.map(({ icon, title, footer, ...rest }) => (
           <StatisticsCard
             key={title}
@@ -58,7 +58,8 @@ export default function Dashboard() {
             }
           />
         ))}
-      </div>
+      </div> */}
+      dashboard
     </div>
   );
 }
