783feb4a53765cd6dd5408d8ebc23f5bb7d62939
dinh <dinhtq170601@gmail.com>
2023-11-27 23:45:35 +0700

    fix call stack

--- src/app/admin/dashboard/Chart/page.js
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const ChartDynamic = dynamic(
-  () => import('./statistics-chart'),
-  { ssr: false }
-)
-export default function Chart() {
-  return <div><ChartDynamic /></div>
-}
