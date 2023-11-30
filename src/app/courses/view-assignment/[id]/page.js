30329ccd336b5c1f2c6c13174928b5a43bc8ebdb
dinh <dinhtq170601@gmail.com>
2023-11-30 15:29:51 +0700

    fix call stack size

--- src/app/courses/view-assignment/[id]/page.js
+++ src/app/courses/view-assignment/[id]/page.js
@@ -1,291 +1,164 @@
 "use client";
-import React, { useCallback, useEffect, useState } from "react";
-import { Card, Radio, message, Row, Col, Statistic, Spin } from "antd";
-import { unwrapResult } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
-import { useRouter } from "next/navigation";
-import {
-  submitAssignment,
-  viewAssignmentByCourseId,
-} from "@/features/Assignment/assignmentSlice";
+import { viewAssignmentByCourseId } from "@/features/Assignment/assignmentSlice";
 import { getScore } from "@/features/Quiz/quizSlice";
+import { unwrapResult } from "@reduxjs/toolkit";
+import { Button } from "antd";
 import Link from "next/link";
-import { Button } from "@material-tailwind/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 
-export default function Assignment({ params }) {
-  const dispatch = useDispatch();
-  const [selectedAnswers, setSelectedAnswers] = useState({});
+export default function HandleStart({ params }) {
   const [assignment, setAssigment] = useState([]);
-  const [messageApi, contextHolder] = message.useMessage();
-  const [submitted, setSubmitted] = useState(false);
-  const [started, setStarted] = useState(false);
-  const [timeLeft, setTimeLeft] = useState(null);
-  const [startTime, setStartTime] = useState(null);
+  console.log("🚀 ~ assignment:", assignment);
   const [score, setScore] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
+  const dispatch = useDispatch();
   const router = useRouter();
 
-  // const handleAnswer = (questionId, answer) => {
-  //   setSelectedAnswers((prevAnswers) => ({
-  //     ...prevAnswers,
-  //     [questionId]: answer,
-  //   }));
+  // const handleStart = async () => {
+  //   console.log("handleStart function has started");
+  //   dispatch(viewAssignmentByCourseId({ courseId: params?.id }))
+  //     .then(unwrapResult)
+  //     .then((res) => {
+  //       console.log("🚀 ~ res:", res);
+  //       if (res.status) {
+  //         router.push(`/courses/view-assignment/list-assignment/${params?.id}`);
+  //       } else {
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  //   console.log("handleStart function has finished");
   // };
 
-  const handleAnswer = useCallback((questionId, answer) => {
-    console.log("handleAnswer function has started");
-    setSelectedAnswers((prevAnswers) => ({
-      ...prevAnswers,
-      [questionId]: answer,
-    }));
-    console.log("handleAnswer function has finished");
-  }, []);
+  // useEffect(() => {
+  //   console.log("handleStart useEffect function has started");
+  //   dispatch(viewAssignmentByCourseId({ courseId: params?.id }))
+  //     .then(unwrapResult)
+  //     .then((res) => {
+  //       console.log("🚀 ~ res:", res);
+  //       if (res.status) {
+  //         setAssigment(res.metadata);
+  //       } else {
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  //   console.log("handleStart useEffect function has finished");
 
-  useEffect(() => {
-    console.log("useEffect has started");
-    setIsLoading(true);
-    Promise.all([
-      dispatch(viewAssignmentByCourseId({ courseId: params?.id })),
-      dispatch(getScore()),
-    ])
-      .then(([assignmentRes, scoreRes]) => {
-        const assignmentResult = assignmentRes.payload;
-        const scoreResult = scoreRes.payload;
+  //   console.log("getScore function has started");
+  //   dispatch(getScore())
+  //     .then(unwrapResult)
+  //     .then((res) => {
+  //       if (res.status) {
+  //         setScore(res.metadata);
+  //       } else {
+  //         messageApi.error(res.message);
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  //   console.log("getScore function has finished");
+  // }, []);
 
-        if (assignmentResult.status) {
-          setAssigment(assignmentResult.metadata);
-          setTimeLeft(assignmentResult.metadata[0]?.timeLimit * 60);
-        } else {
-          messageApi.error(assignmentResult.message);
-        }
-
-        if (scoreResult.status) {
-          setScore(scoreResult.metadata);
-        } else {
-          messageApi.error(scoreResult.message);
-        }
-
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.log(error);
-        setIsLoading(false);
-      });
-    console.log("useEffect has finished");
-  }, [dispatch, params?.id, messageApi]);
-
-  const handleCountdownFinish = () => {
-    if (!submitted) {
-      handleSubmit();
+  const handleStart = async () => {
+    try {
+      const res = await dispatch(
+        viewAssignmentByCourseId({ courseId: params?.id })
+      ).then(unwrapResult);
+      if (res.status) {
+        router.push(`/courses/view-assignment/list-assignment/${params?.id}`);
+      }
+    } catch (error) {
+      console.error(error);
     }
   };
 
-  const handleStart = async () => {
-    console.log("handleStart function has started");
-    setStarted(true);
-    setStartTime(Date.now());
-    dispatch(viewAssignmentByCourseId({ courseId: params?.id }))
-      .then(unwrapResult)
-      .then((res) => {
+  useEffect(() => {
+    const fetchAssignmentAndScore = async () => {
+      try {
+        const res = await dispatch(
+          viewAssignmentByCourseId({ courseId: params?.id })
+        ).then(unwrapResult);
         if (res.status) {
           setAssigment(res.metadata);
-          setTimeLeft(res.metadata[0].timeLimit * 60);
-        } else {
-          messageApi.error(res.message);
         }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    console.log("handleStart function has finished");
-  };
 
-  const handleSubmit = async () => {
-    const expectedEndTime = startTime + timeLeft * 1000;
-    const actualEndTime = Date.now();
-    const endTime = Math.min(expectedEndTime, actualEndTime); // Use the earlier of the two times
-    const timeTaken = Math.floor((endTime - startTime) / 1000); // in milliseconds
-
-    const formattedAnswers = Object.entries(selectedAnswers).map(
-      ([questionId, answer]) => ({
-        [questionId]: answer,
-      })
-    );
-    dispatch(
-      submitAssignment({
-        assignmentId: assignmentId,
-        answer: formattedAnswers,
-        timeLimit: timeTaken,
-      })
-    )
-      .then(unwrapResult)
-      .then((res) => {
-        console.log("submitAssignment response received");
-        if (res.status) {
-          messageApi
-            .open({
-              type: "success",
-              content: "Action in progress...",
-              duration: 2.5,
-            })
-            .then(() => {
-              setSubmitted(true);
-              router.push("/courses/view-course");
-            })
-            .then(() => message.success(res.message, 1.5));
-        } else {
-          messageApi.error(res.message);
+        const scoreRes = await dispatch(getScore()).then(unwrapResult);
+        if (scoreRes.status) {
+          setScore(scoreRes.metadata);
         }
-      })
-      .catch((error) => {
-        console.log("submitAssignment error:", error);
-      });
-  };
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchAssignmentAndScore();
+  }, []);
 
   const assignmentId = assignment[0]?._id;
   const currentScore = score.find((s) => s.assignment?._id === assignmentId);
 
-  return (
-    <div>
-      {contextHolder}
-      {isLoading ? (
-        <div className="flex justify-center items-center h-screen">
-          <Spin />
+  return score?.some(
+    (s) => s?.assignment?._id === assignmentId && s?.isComplete
+  ) ? (
+    <div className="flex items-center justify-center">
+      <div className="rounded-lg bg-gray-50 px-16 py-14">
+        <div className="flex justify-center">
+          <div className="rounded-full bg-green-200 p-6">
+            <div className="flex h-16 w-16 items-center justify-center rounded-full bg-green-500 p-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-8 w-8 text-white"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 12.75l6 6 9-13.5"
+                />
+              </svg>
+            </div>
+          </div>
         </div>
-      ) : (
-        <Row
-          style={{
-            paddingBottom: "200px",
-            overflow: "auto",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
+        <h3 className="my-4 text-center text-3xl font-semibold text-gray-700">
+          Complete!!!
+        </h3>
+        <p className="w-[230px] text-center font-normal text-gray-600">
+          You finished with a score: {currentScore?.score}
+        </p>
+        <div>
+          <Link href="/courses/view-course">
+            <div className="mx-auto mt-10 block rounded-xl border-4 border-transparent bg-orange-400 px-6 py-3 text-center text-base font-medium text-orange-100 outline-8 hover:outline hover:duration-300">
+              Home
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="flex items-center justify-center">
+      <div className="rounded-lg bg-gray-50 px-16 py-14 items-center justify-center">
+        <h3 className="my-4 text-center text-3xl font-semibold text-gray-700">
+          Test!!!
+        </h3>
+        <p className="w-[230px] text-center font-bold text-red-600">
+          Please do not exit at the beginning of the test!!!
+        </p>
+        <Button
+          type="primary"
+          onClick={handleStart}
+          className="mx-auto mt-10 block rounded-xl border-4 border-transparent bg-orange-400 px-6 py-3 text-center text-base font-medium text-orange-100 outline-8 hover:outline hover:duration-300"
         >
-          <Col xs={24} md={16}>
-            {/* {score?.some(
-              (s) => s?.assignment?._id === assignmentId && s?.isComplete
-            ) ? (
-              <div className="flex items-center justify-center">
-                <div className="rounded-lg bg-gray-50 px-16 py-14">
-                  <div className="flex justify-center">
-                    <div className="rounded-full bg-green-200 p-6">
-                      <div className="flex h-16 w-16 items-center justify-center rounded-full bg-green-500 p-4">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="h-8 w-8 text-white"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M4.5 12.75l6 6 9-13.5"
-                          />
-                        </svg>
-                      </div>
-                    </div>
-                  </div>
-                  <h3 className="my-4 text-center text-3xl font-semibold text-gray-700">
-                    Complete!!!
-                  </h3>
-                  <p className="w-[230px] text-center font-normal text-gray-600">
-                    You finished with a score: {currentScore?.score}
-                  </p>
-                  <div>
-                    <Link href="/courses/view-course">
-                      <div className="mx-auto mt-10 block rounded-xl border-4 border-transparent bg-orange-400 px-6 py-3 text-center text-base font-medium text-orange-100 outline-8 hover:outline hover:duration-300">
-                        Home
-                      </div>
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            ) : started ? (
-              assignment?.map((item, index) => (
-                <React.Fragment key={item._id}>
-                  <Card title={item.name}>
-                    <Statistic.Countdown
-                      value={
-                        startTime + (timeLeft !== null ? timeLeft * 1000 : 0)
-                      }
-                      onFinish={handleCountdownFinish}
-                      format="mm:ss"
-                    />
-                    {item?.questions?.map((question, questionIndex) => {
-                      const isCorrectAnswer =
-                        selectedAnswers[question._id] === question.answer;
-                      const showAnswer = submitted && isCorrectAnswer;
-                      const showWrongAnswer = submitted && !isCorrectAnswer;
-                      return (
-                        <div key={question._id}>
-                          <h4
-                            style={{
-                              marginBottom: "10px",
-                              color: showAnswer
-                                ? "green"
-                                : showWrongAnswer
-                                ? "red"
-                                : "black",
-                            }}
-                          >
-                            Question {index + 1}.{questionIndex + 1}:{" "}
-                            {question.question}
-                            {showAnswer && " ✔️"}
-                            {showWrongAnswer && "❌"}
-                          </h4>
-                          <Radio.Group
-                            onChange={(e) =>
-                              handleAnswer(question._id, e.target.value)
-                            }
-                            disabled={submitted}
-                          >
-                            {question.options.map((option) => (
-                              <div key={option}>
-                                <Radio value={option}>{option}</Radio>
-                              </div>
-                            ))}
-                          </Radio.Group>
-                        </div>
-                      );
-                    })}
-                  </Card>
-                  <div style={{ padding: "1rem" }}>
-                    <Button
-                      type="primary"
-                      onClick={handleSubmit}
-                      className="button-container me-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                    >
-                      Submit
-                    </Button>
-                  </div>
-                </React.Fragment>
-              ))
-            ) : (
-              <div className="flex items-center justify-center">
-                <div className="rounded-lg bg-gray-50 px-16 py-14 items-center justify-center">
-                  <h3 className="my-4 text-center text-3xl font-semibold text-gray-700">
-                    Test!!!
-                  </h3>
-                  <p className="w-[230px] text-center font-bold text-red-600">
-                    Please do not exit at the beginning of the test!!!
-                  </p>
-                  <Button
-                    type="primary"
-                    onClick={handleStart}
-                    className="mx-auto mt-10 block rounded-xl border-4 border-transparent bg-orange-400 px-6 py-3 text-center text-base font-medium text-orange-100 outline-8 hover:outline hover:duration-300"
-                  >
-                    Start
-                  </Button>
-                </div>
-              </div>
-            )} */}
-            <h1>HI Assignment</h1>
-          </Col>
-        </Row>
-      )}
+          Start
+        </Button>
+      </div>
     </div>
   );
 }
---
+++ src/app/courses/view-assignment/list-assignment/[id]/page.js
@@ -0,0 +1,188 @@
+"use client";
+import React, { useCallback, useEffect, useState } from "react";
+import { Card, Radio, message, Row, Col, Statistic, Spin } from "antd";
+import { unwrapResult } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+import { useRouter } from "next/navigation";
+import {
+  submitAssignment,
+  viewAssignmentByCourseId,
+} from "@/features/Assignment/assignmentSlice";
+import { Button } from "@material-tailwind/react";
+
+export default function ListAssignment({ params }) {
+  const dispatch = useDispatch();
+  const [selectedAnswers, setSelectedAnswers] = useState({});
+  const [assignment, setAssigment] = useState([]);
+  const [messageApi, contextHolder] = message.useMessage();
+  const [submitted, setSubmitted] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(null);
+  const [startTime, setStartTime] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const router = useRouter();
+
+  const handleAnswer = useCallback((questionId, answer) => {
+    console.log("handleAnswer function has started");
+    setSelectedAnswers((prevAnswers) => ({
+      ...prevAnswers,
+      [questionId]: answer,
+    }));
+    console.log("handleAnswer function has finished");
+  }, []);
+
+  useEffect(() => {
+    console.log("useEffect has started");
+    setIsLoading(true);
+    setStartTime(Date.now());
+    dispatch(viewAssignmentByCourseId({ courseId: params?.id }))
+      .then(unwrapResult)
+      .then((res) => {
+        console.log("🚀 ~ res:", res);
+        if (res.status) {
+          setAssigment(res.metadata);
+          setTimeLeft(res.metadata[0]?.timeLimit * 60);
+        } else {
+          messageApi.error(res.message);
+        }
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+    console.log("useEffect has finished");
+  }, [dispatch, params?.id, messageApi]);
+
+  const handleCountdownFinish = () => {
+    if (!submitted) {
+      handleSubmit();
+    }
+  };
+
+  const assignmentId = assignment[0]?._id;
+
+  const handleSubmit = async () => {
+    const expectedEndTime = startTime + timeLeft * 1000;
+    const actualEndTime = Date.now();
+    const endTime = Math.min(expectedEndTime, actualEndTime); // Use the earlier of the two times
+    const timeTaken = Math.floor((endTime - startTime) / 1000); // in milliseconds
+
+    const formattedAnswers = Object.entries(selectedAnswers).map(
+      ([questionId, answer]) => ({
+        [questionId]: answer,
+      })
+    );
+    dispatch(
+      submitAssignment({
+        assignmentId: assignmentId,
+        answer: formattedAnswers,
+        timeLimit: timeTaken,
+      })
+    )
+      .then(unwrapResult)
+      .then((res) => {
+        console.log("submitAssignment response received");
+        if (res.status) {
+          messageApi
+            .open({
+              type: "success",
+              content: "Action in progress...",
+              duration: 2.5,
+            })
+            .then(() => {
+              setSubmitted(true);
+              router.push("/courses/view-course");
+            })
+            .then(() => message.success(res.message, 1.5));
+        } else {
+          messageApi.error(res.message);
+        }
+      })
+      .catch((error) => {
+        console.log("submitAssignment error:", error);
+      });
+  };
+
+  return (
+    <div>
+      {contextHolder}
+      {isLoading ? (
+        <div className="flex justify-center items-center h-screen">
+          <Spin />
+        </div>
+      ) : (
+        <Row
+          style={{
+            paddingBottom: "200px",
+            overflow: "auto",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Col xs={24} md={16}>
+            {assignment?.map((item, index) => (
+              <React.Fragment key={item._id}>
+                <Card title={item.name}>
+                  <Statistic.Countdown
+                    value={
+                      startTime + (timeLeft !== null ? timeLeft * 1000 : 0)
+                    }
+                    onFinish={handleCountdownFinish}
+                    format="mm:ss"
+                  />
+                  {item?.questions?.map((question, questionIndex) => {
+                    const isCorrectAnswer =
+                      selectedAnswers[question._id] === question.answer;
+                    const showAnswer = submitted && isCorrectAnswer;
+                    const showWrongAnswer = submitted && !isCorrectAnswer;
+                    return (
+                      <div key={question._id}>
+                        <h4
+                          style={{
+                            marginBottom: "10px",
+                            color: showAnswer
+                              ? "green"
+                              : showWrongAnswer
+                              ? "red"
+                              : "black",
+                          }}
+                        >
+                          Question {index + 1}.{questionIndex + 1}:{" "}
+                          {question.question}
+                          {showAnswer && " ✔️"}
+                          {showWrongAnswer && "❌"}
+                        </h4>
+                        <Radio.Group
+                          onChange={(e) =>
+                            handleAnswer(question._id, e.target.value)
+                          }
+                          disabled={submitted}
+                        >
+                          {question.options.map((option) => (
+                            <div key={option}>
+                              <Radio value={option}>{option}</Radio>
+                            </div>
+                          ))}
+                        </Radio.Group>
+                      </div>
+                    );
+                  })}
+                </Card>
+                <div style={{ padding: "1rem" }}>
+                  <Button
+                    type="primary"
+                    onClick={handleSubmit}
+                    className="button-container me-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Submit
+                  </Button>
+                </div>
+              </React.Fragment>
+            ))}
+          </Col>
+        </Row>
+      )}
+    </div>
+  );
+}
