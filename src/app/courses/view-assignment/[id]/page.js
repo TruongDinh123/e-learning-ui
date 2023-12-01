38277ed2b659087c4b84c8599ff4f95489e46c00
dinh <dinhtq170601@gmail.com>
2023-12-01 07:01:49 +0700

    deploy production

--- src/app/courses/view-assignment/[id]/page.js
+++ src/app/courses/view-assignment/[id]/page.js
@@ -1,19 +1,18 @@
 "use client";
 import { viewAssignmentByCourseId } from "@/features/Assignment/assignmentSlice";
 import { getScore } from "@/features/Quiz/quizSlice";
-import { Button } from "@material-tailwind/react";
 import { unwrapResult } from "@reduxjs/toolkit";
-import { Spin } from "antd";
+import { Button, Spin } from "antd";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 
 export default function HandleStart({ params }) {
-  const [assignment, setAssigment] = useState([]);
-  const [score, setScore] = useState([]);
-  const dispatch = useDispatch();
-  const router = useRouter();
+  // const [assignment, setAssigment] = useState([]);
+  // const [score, setScore] = useState([]);
+  // const dispatch = useDispatch();
+  // const router = useRouter();
 
   // const handleStart = async () => {
   //   try {
@@ -80,25 +79,8 @@ export default function HandleStart({ params }) {
   //   fetchData();
   // }, [dispatch, params?.id]);
 
-  const assignmentId = assignment[0]?._id;
-  const currentScore = score.find((s) => s.assignment?._id === assignmentId);
+  // const assignmentId = assignment[0]?._id;
+  // const currentScore = score.find((s) => s.assignment?._id === assignmentId);
 
-  return (
-    <div className="flex items-center justify-center">
-      <div className="rounded-lg bg-gray-50 px-16 py-14 items-center justify-center">
-        <h3 className="my-4 text-center text-3xl font-semibold text-gray-700">
-          Test!!!
-        </h3>
-        <p className="w-[230px] text-center font-bold text-red-600">
-          Please do not exit at the beginning of the test!!!
-        </p>
-        <Button
-          type="primary"
-          className="mx-auto mt-10 block rounded-xl border-4 border-transparent bg-orange-400 px-6 text-center text-base font-medium text-orange-100 outline-8 hover:outline hover:duration-300"
-        >
-          Start
-        </Button>
-      </div>
-    </div>
-  );
+  return <h1>Hello</h1>;
 }
