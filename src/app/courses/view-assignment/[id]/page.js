3464b65e7d3b7378e65f1e49336ffbee3e974805
dinh <dinhtq170601@gmail.com>
2023-12-01 11:57:06 +0700

    test bug call stack

--- src/app/courses/view-assignment/[id]/page.js
+++ src/app/courses/view-assignment/[id]/page.js
@@ -25,81 +25,31 @@ export default function HandleStart({ params }) {
   //   }
   // };
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     setIsLoading(true);
-  //     try {
-  //       const [res, scoreRes] = await Promise.all([
-  //         dispatch(viewAssignmentByCourseId({ courseId: params?.id })).then(
-  //           unwrapResult
-  //         ),
-  //         dispatch(getScore()).then(unwrapResult),
-  //       ]);
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log('fetchData is called');
+      try {
+        const assignmentPromise = dispatch(
+          viewAssignmentByCourseId({ courseId: params?.id })
+        ).then(unwrapResult);
+        const scorePromise = dispatch(getScore()).then(unwrapResult);
 
-  //       if (res.status) {
-  //         setAssigment(res.metadata);
-  //       }
-  //       if (scoreRes.status) {
-  //         setScore(scoreRes.metadata);
-  //       }
-  //     } catch (error) {
-  //       console.error(error);
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   };
+        const res = await assignmentPromise;
+        if (res.status) {
+          setAssigment(res.metadata);
+        }
 
-  //   fetchData();
-  // }, [dispatch, params?.id]);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const assignmentPromise = dispatch(
-  //         viewAssignmentByCourseId({ courseId: params?.id })
-  //       ).then(unwrapResult);
-  //       const scorePromise = dispatch(getScore()).then(unwrapResult);
-
-  //       const res = await assignmentPromise;
-  //       if (res.status) {
-  //         setAssigment(res.metadata);
-  //       }
-
-  //       const scoreRes = await scorePromise;
-  //       if (scoreRes.status) {
-  //         setScore(scoreRes.metadata);
-  //       }
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, [params?.id]);
-
-
-  const fetchData = async () => {
-    try {
-      const assignmentPromise = dispatch(
-        viewAssignmentByCourseId({ courseId: params?.id })
-      ).then(unwrapResult);
-      const scorePromise = dispatch(getScore()).then(unwrapResult);
-
-      const res = await assignmentPromise;
-      if (res.status) {
-        setAssigment(res.metadata);
+        const scoreRes = await scorePromise;
+        if (scoreRes.status) {
+          setScore(scoreRes.metadata);
+        }
+      } catch (error) {
+        console.error(error);
       }
+    };
 
-      const scoreRes = await scorePromise;
-      if (scoreRes.status) {
-        setScore(scoreRes.metadata);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  fetchData();
+    fetchData();
+  }, [dispatch, params?.id]);
 
   const assignmentId = assignment[0]?._id;
   const currentScore = score.find((s) => s.assignment?._id === assignmentId);
