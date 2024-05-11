db15b32f126ae4d3d1353e97b8f11630197c6279
Phan Thi Mai <mai-phan@cybozu.vn>
2024-05-11 15:00:39 +0700

    Add logo and audit code

--- src/app/user/exem-online/content-exem-online/page.js
+++ src/app/user/exem-online/content-exem-online/page.js
@@ -133,41 +133,41 @@ export default function ContentExemplOnline({params}) {
         </div>
       </section>
       <div className='container mx-auto px-2 lg:px-4 mt-6 lg:mt-16'>
-        <section id='blog'>
-          <div className='text-[#002c6a] text-center text-xl lg:text-4xl font-bold'>
-            THÔNG BÁO BAN TỔ CHỨC
-          </div>
-          <div className='my-6 lg:my-12'>
-            <div className='flex items-center gap-8'>
-              <div className='basis-1/2 hidden lg:block'>
-                <img
-                  src='https://myaloha.vn/upload/images/banner/banner_1011571_1684482408_ceafed9c-c81f-4aaa-9abb-3b4954db9d5f.png'
-                  alt
-                  className='object-contain rounded-lg shadow w-full h-auto'
-                />
-              </div>
-              <div className='grow lg:basis-1/2'>
-                <ul>
-                  <li>
-                    <a
-                      href=''
-                      className='flex items-center gap-6 border-slate-300 border-t border-b px-2 py-4'
-                    >
-                      <div className='shrink-0 text-theme-color text-sm'>
-                        5-4
-                      </div>
-                      <div className='w-2 h-2 bg-slate-300 rounded-full'></div>
-                      <div className='grow line-clamp-1'>
-                        TÀI LIỆU SHHV: NGHỊ QUYẾT ĐẠI HỘI PHỤ NỮ TOÀN QUỐC LẦN
-                        THỨ XIII
-                      </div>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </section>
+        {/*<section id='blog'>*/}
+        {/*  <div className='text-[#002c6a] text-center text-xl lg:text-4xl font-bold'>*/}
+        {/*    THÔNG BÁO BAN TỔ CHỨC*/}
+        {/*  </div>*/}
+        {/*  <div className='my-6 lg:my-12'>*/}
+        {/*    <div className='flex items-center gap-8'>*/}
+        {/*      <div className='basis-1/2 hidden lg:block'>*/}
+        {/*        <img*/}
+        {/*          src='https://myaloha.vn/upload/images/banner/banner_1011571_1684482408_ceafed9c-c81f-4aaa-9abb-3b4954db9d5f.png'*/}
+        {/*          alt*/}
+        {/*          className='object-contain rounded-lg shadow w-full h-auto'*/}
+        {/*        />*/}
+        {/*      </div>*/}
+        {/*      <div className='grow lg:basis-1/2'>*/}
+        {/*        <ul>*/}
+        {/*          <li>*/}
+        {/*            <a*/}
+        {/*              href=''*/}
+        {/*              className='flex items-center gap-6 border-slate-300 border-t border-b px-2 py-4'*/}
+        {/*            >*/}
+        {/*              <div className='shrink-0 text-theme-color text-sm'>*/}
+        {/*                5-4*/}
+        {/*              </div>*/}
+        {/*              <div className='w-2 h-2 bg-slate-300 rounded-full'></div>*/}
+        {/*              <div className='grow line-clamp-1'>*/}
+        {/*                TÀI LIỆU SHHV: NGHỊ QUYẾT ĐẠI HỘI PHỤ NỮ TOÀN QUỐC LẦN*/}
+        {/*                THỨ XIII*/}
+        {/*              </div>*/}
+        {/*            </a>*/}
+        {/*          </li>*/}
+        {/*        </ul>*/}
+        {/*      </div>*/}
+        {/*    </div>*/}
+        {/*  </div>*/}
+        {/*</section>*/}
       </div>
     </main>
   );
