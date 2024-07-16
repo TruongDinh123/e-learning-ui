a56b65f01fb288ca137a97c79afde479485356ec
hoangtran2506 <117815925+hoangtran2506@users.noreply.github.com>
2024-07-16 16:22:44 +0700

    [chore]: disable button join

--- src/app/user/exem-online/content-exem-online/countdownFooter.js
+++ src/app/user/exem-online/content-exem-online/countdownFooter.js
@@ -66,6 +66,7 @@ const CountdownFooter = ({testCount, timeSubmission}) => {
             if (userCurrent && timeSubmission.checkTime) confirmStartQuiz();
             if (!userCurrent) router.push('/login');
           }}
+          disabled={true}
         >
           Tham gia
         </button>
